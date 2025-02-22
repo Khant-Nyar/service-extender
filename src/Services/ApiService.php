3bb840b78081bdd01a426ab6862bd01e7594dfac
Khant-Nyar <khantnyar.dev@gmail.com>
2025-02-22 17:31:23 +0630

    [impl:api] find crud

--- src/Services/ApiService.php
+++ src/Services/ApiService.php
@@ -8,26 +8,47 @@ use KhantNyar\ServiceExtender\Services\Contracts\ThirpartyApiInterface;
 abstract class ApiService implements ThirpartyApiInterface
 {
     protected static string $base;
+    protected static string $apiKey;
 
-    protected static string $api_key;
-
-    public function build(): void
+    protected function makeRequest(string $method, string $endpoint, array $data = [])
     {
-        /** Build Api CLinet with $base url */
+        $url = rtrim(static::$base, '/') . '/' . ltrim($endpoint, '/');
+
+        $response = Http::withHeaders([
+            'Authorization' => 'Bearer ' . static::$apiKey,
+            'Accept' => 'application/json',
+            'Content-Type' => 'application/json',
+        ])->$method($url, $data);
+
+        return $this->handleResponse($response);
     }
 
-    public function make(): void
+    protected function handleResponse($response)
     {
-        // return Http::
+        if ($response->successful()) {
+            return $response->json();
+        }
+
+        throw new \Exception("API request failed: " . $response->body(), $response->status());
     }
 
-    public function get(): void
+    public function get(string $endpoint, array $query = [])
     {
-        $this->make();
+        return $this->makeRequest('get', $endpoint, $query);
     }
 
-    public function post(): void
+    public function post(string $endpoint, array $data)
     {
-        $this->make();
+        return $this->makeRequest('post', $endpoint, $data);
+    }
+
+    public function put(string $endpoint, array $data)
+    {
+        return $this->makeRequest('put', $endpoint, $data);
+    }
+
+    public function delete(string $endpoint, array $data = [])
+    {
+        return $this->makeRequest('delete', $endpoint, $data);
     }
 }
--- src/Services/Contracts/ThirpartyApiInterface.php
+++ src/Services/Contracts/ThirpartyApiInterface.php
@@ -3,5 +3,11 @@
 namespace KhantNyar\ServiceExtender\Services\Contracts;
 
 interface ThirpartyApiInterface {
-    // public function 
+    public function get(string $endpoint, array $query = []);
+
+    public function post(string $endpoint, array $data);
+
+    public function put(string $endpoint, array $data);
+
+    public function delete(string $endpoint, array $data = []);
 }
